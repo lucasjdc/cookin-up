80c6b4ba0e82ac5ce7d2ff9127bb87cc6b37617c
Lucas Costa <lucasjpdc@gmail.com>
2026-03-19 17:49:30 -0300

    Add key binding to v-for list items

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -16,7 +16,7 @@ export default {
                 Sua lista:
             </span>
             <ul class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" class="ingrediente">{{ ingrediente }}</li>                
+                <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">{{ ingrediente }}</li>
             </ul>
         </section>
 
