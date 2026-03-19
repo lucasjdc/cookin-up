46b23efcfe8f28d2193b3a96144794e2a76c5dd1
Lucas Costa <lucasjpdc@gmail.com>
2026-03-19 01:28:06 -0300

    remove default Vue components and start main screen

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
 </script>
 
 <template>
-  <Banner />  
+  <Banner />
+  <ConteudoPrincipal />
 </template>
-
-<style scoped>
-
-</style>
